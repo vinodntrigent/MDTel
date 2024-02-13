437a9467e4e95122ea498a7f06c25b2f2dc77290
Harrshad <harrshad1599@gmail.com>
2024-02-13 18:11:43 +0530

    SRS verification of content in Insurance and other pages were done

--- assets/js/case-notes-add.js
+++ assets/js/case-notes-add.js
@@ -42,7 +42,103 @@ $(document).ready(function () {
             $(".ayd3").hide();
            });
 
-    
+           $(".check-ayd4").on("click", function (){
+            $(".ayd4").show();
+    });
+    $(".check-ayd4no, .check-ayd4na ").on("click", function (){
+            $(".ayd4").hide();
+           });
+
+           $(".check-ayd6").on("click", function (){
+            $(".ayd6").show();
+    });
+    $(".check-ayd6no, .check-ayd6na ").on("click", function (){
+            $(".ayd6").hide();
+           });
+
+           $(".check-ayd7").on("click", function (){
+            $(".ayd7").show();
+    });
+    $(".check-ayd7no, .check-ayd7na ").on("click", function (){
+            $(".ayd7").hide();
+           });
+
+           $(".check-aymnyes").on("click", function (){
+            $(".aymnyes").show();
+    });
+    $(".check-aymnno").on("click", function (){
+            $(".aymnyes").hide();
+           });
+
+           $(".check-ayc").on("click", function (){
+            $(".aycno").show();
+    });
+    $(".check-aycno, .check-aycna").on("click", function (){
+            $(".aycno").hide();
+           });
+
+           $(".check-aym1").on("click", function (){
+            $(".aymyes").show();
+    });
+    $(".check-aym1no, .check-aym1na").on("click", function (){
+            $(".aymyes").hide();
+           });
+
+           $(".check-aomn1").on("click", function (){
+            $(".aomn1").show();
+    });
+    $(".check-aomn1no, .check-aomn1na").on("click", function (){
+            $(".aomn1").hide();
+           });
+
+           $(".check-aomn2").on("click", function (){
+            $(".aomn2").show();
+    });
+    $(".check-aomn2no, .check-aomn2na").on("click", function (){
+            $(".aomn2").hide();
+           });
+
+           $(".check-aomn3").on("click", function (){
+            $(".aomn3").show();
+    });
+    $(".check-aomn3no, .check-aomn3na").on("click", function (){
+            $(".aomn3").hide();
+           });
+
+           $(".check-sdoh15").on("click", function (){
+            $(".sdoh15yes").show();
+    });
+    $(".check-sdoh15no, .check-sdoh15dec, .check-sdoh15na").on("click", function (){
+            $(".sdoh15yes").hide();
+           });
+
+           $(".check-sdoh12").on("click", function (){
+            $(".sdoh12yes").show();
+    });
+    $(".check-sdoh12no, .check-sdoh12dec, .check-sdoh12na").on("click", function (){
+            $(".sdoh12yes").hide();
+           });
+
+           $(".check-sdoh11").on("click", function (){
+            $(".sdoh11yes").show();
+    });
+    $(".check-sdoh11no, .check-sdoh11dec, .check-sdoh11na").on("click", function (){
+            $(".sdoh11yes").hide();
+           });
+
+           $(".check-ypn18").on("click", function (){
+            $(".ypn18").show();
+    });
+    $(".check-ypn18no, .check-ypn18na").on("click", function (){
+            $(".ypn18").hide();
+           });
+
+           $(".btn-vital").on("click", function(){
+            $(".new-table").show();
+           });
+           $(".btn-vital-close").on("click", function(){
+            $(".new-table").hide();
+           });
     // $("input[name='toggle']").on("click", function () {
     //     var checkedRadioId = $("input[name='toggle']:checked").attr("id");
     
